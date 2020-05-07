573a59516e9d11349d2ae829f55599d63a7ee85b
Farhad Kazemi <farhad@example.com>
2020-05-07 22:00:57 +1000

    added more unit tests for location entity

--- Location.test.js
+++ Location.test.js
@@ -1,6 +1,6 @@
 const Location = require('./Location')
 
-test('Location can be created from an object', () => {
+test('Location lng validation', () => {
 
     try {
         Location.from({lng: -600, lat: 30, address: 'something more than 10 characters'})
@@ -8,6 +8,19 @@ test('Location can be created from an object', () => {
 
     } catch (e) {
         expect(e.message).toBe('lng must be greater than or equal to -180')
+        return
+    }
+    throw new Error('Expected to file')
+});
+
+test('Location lon validation', () => {
+
+    try {
+        Location.from({lng: -60, lat: 91, address: 'something more than 10 characters'})
+            .validate()
+
+    } catch (e) {
+        expect(e.message).toBe('lat must be less than or equal to 90')
         return
     }
     throw new Error('Expected to file')
