883bac9d5efd6819aed924c9f235bc349b725a73
Farhad Kazemi <farhad@example.com>
2020-05-13 20:10:45 +1000

    added plan options

---
+++ Plan.js
@@ -0,0 +1,74 @@
+const yup = require('yup')
+
+/**
+ * Represents a plan that users can purchase in Magny
+ * @class
+ */
+class Plan {
+    /**
+     * Name of the plan to be shown to the user
+     * @type {string} */
+    name;
+    /**
+     * Id of the plan
+     * @readonly
+     * @type {string} */
+    id;
+
+    /**
+     * Features of the plan
+     * @type {string[]} */
+    features;
+
+    /** @type {number} */
+    price;
+
+    /**
+     * Extra features that users can choose.
+     * @type {Array} */
+    options;
+
+    /**
+     * Schema to validate the object
+     */
+    static schema = yup.object().shape({
+        id: yup.string().required(),
+        name: yup.string().required(),
+        features: yup.array(yup.string()),
+        price: yup.number().positive().required(),
+        options: yup.array(yup.object().shape({
+            id: yup.string().required(),
+            title: yup.string(),
+            price: yup.number().positive().required()
+        }))
+
+    });
+
+    /**
+     *
+     * @param prop properties to populate the class from
+     * @returns {Plan} a proper instantiated class
+     */
+    static from(prop) {
+        return Object.assign(new Plan(), prop)
+    }
+
+    /**
+     * Validates the object
+     * @returns {*} Casted version of this object or throws Validation errors
+     */
+    validate() {
+        return Plan.schema.validateSync(this);
+    }
+
+    /**
+     *
+     * @param obj Object to be validated against the class's schema
+     * @returns {*} list of validation errors or a casted version of the obj
+     */
+    static validateObject(obj) {
+        return Plan.schema.validateSync(obj);
+    }
+}
+
+module.exports = Plan
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Business = require('./Business')
 const Location = require('./Location')
+const Plan = require('./Plan')
 module.exports = {
     Business,
-    Location
+    Location,
+    Plan
 }
